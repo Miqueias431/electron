e00ae801b5d04184e7daadc9fcd705c2c82cd9a1
Miqueias Rodrigues Barreto <miqueiasr560@gmail.com>
2024-06-07 15:03:14 -0300

    add+

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 // console.log("Processo Principal")
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, nativeTheme } = require('electron')
 
+// Janela Principal
 const createWindow = () => {
+
+  // nativeTheme.themeSource = 'light'
+
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 800,  // Largura
+    height: 600,  // Altura
+    icon: './src/public/img/pc.png'
+    // resizable: false, // Evitar o redimensionameto
+    // titleBarStyle: 'hidden', // Esconder barra de titulo e menu
+    // autoHideMenuBar: true, // Esconde a barra de menu
+
   })
 
-  win.loadFile('index.html')
+  win.loadFile('./src/views/index.html')
+
 }
 
+// Janela Sobre
+const aboutWindow = () => {
+
+  const about = new BrowserWindow({
+    width: 360,  // Largura
+    height: 220,  // Altura
+    icon: './src/public/img/pc.png',
+    resizable: false, // Evitar o redimensionameto
+    autoHideMenuBar: true, // Esconde a barra de menu
+
+  })
+
+  about.loadFile('./src/views/sobre.html')
+
+}
+
+// Execultar de forma assíncrona a aplicação 
 app.whenReady().then(() => {
   createWindow()
+  aboutWindow()
 })
